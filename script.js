fb3511305b7f7b796ca43dcb909d78cc575f03c8
vishakha-shete <vishakhashete2@gmail.com>
2026-02-01 23:36:06 +0530

    feat: add open/close functionality for feature pages

--- script.js
+++ script.js
@@ -1,7 +1,20 @@
-var allElems = document.querySelectorAll(".elem");
-var allfullElems = document.querySelectorAll('.fullElem')
-allElems.forEach(function(elem){
-    elem.addEventListener('click',function(){
-        allfullElems[elem.id].style.display = 'block'
+function OpenFeatures() {
+    var allElems = document.querySelectorAll(".elem");
+    var FullElemPage = document.querySelectorAll('.fullElem')
+    var FullElemPageBackBtn = document.querySelectorAll('.fullElem .back')
+
+    allElems.forEach(function (elem) {
+        elem.addEventListener('click', function () {
+            FullElemPage[elem.id].style.display = 'block'
+        })
     })
-})
+
+
+    FullElemPageBackBtn.forEach(function (back) {
+        back.addEventListener('click', function () {
+            FullElemPage[back.id].style.display = 'none'
+
+        })
+    })
+}
+OpenFeatures()
