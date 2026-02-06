103be8b29a366446edf15899daf98d3c3d4f8e39
vishakha-shete <vishakhashete2@gmail.com>
2026-02-06 17:08:32 +0530

    Add dynamic task management with array state handling

--- script.js
+++ script.js
@@ -18,3 +18,61 @@ function OpenFeatures() {
     })
 }
 OpenFeatures()
+
+let currentTask = [
+    {
+        task: 'Mandir jaoo',
+        details: 'Hanuman ji vale',
+        imp: 'true',
+    },
+    {
+        task: 'Recording kroo',
+        details: 'cohort ke liyee',
+        imp: 'true',
+    },
+    {
+        task: 'lunch kro 2 baje',
+        details: 'nahi to mummi dategi',
+        imp: 'false',
+    },
+]
+
+function renderTask() {
+    let allTask = document.querySelector('.allTask')
+
+    let sum = ''
+
+
+    currentTask.forEach(function (elem) {
+        sum += `<div class="task">
+    <h5>${elem.task} <span class=${elem.imp}>imp</span></h5>
+    <button>Mark As Completed</button>
+    </div>`
+    })
+    allTask.innerHTML = sum
+
+}
+renderTask()
+
+let form = document.querySelector('.addTask form')
+let taskInput = document.querySelector('.addTask form #task-input')
+let taskDetailsInput = document.querySelector('.addTask form textarea')
+let taskCheckbox = document.querySelector('.addTask form  #check')
+
+form.addEventListener('submit', function (e) {
+    e.preventDefault()
+    currentTask.push(
+        {
+            task: taskInput.value,
+            details: taskDetailsInput.value,
+            imp: taskCheckbox.checked
+        }
+    )
+    taskInput.value = ''
+    taskDetailsInput.value = ''
+    taskCheckbox.checked = false
+
+    renderTask()
+})
+
+
